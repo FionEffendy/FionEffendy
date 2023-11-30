49ab45e76ccebe7241f465d8d88d25dc0e4fe3cd
F10N <fion.foef@gmail.com>
2023-11-30 23:16:55 +0700

    #

---
+++ Alert, Promp, Log/main.js
@@ -0,0 +1,9 @@
+// alert("Semoga Harimu Indah")
+
+console.log('halo usia kamu adalah : ')
+// sintaks di atas di gunakan untuk debuging, caranya di browser klik kanan lalu tekan inspeksi, lalu pilih menu console di sebelah menu element
+
+// prompt('Berpa Usiamu?')
+// menyuruh user untuk memasukkan data di popup
+
+
---
+++ Pembuka/sinau.js
@@ -0,0 +1,1 @@
+alert ("Tetaplah Bahagia Dalam Belajar")
